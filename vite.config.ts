8e977590120f936f83c1776d390e76c301d12a50
Martin Huschenbett <martin.huschenbett@posteo.me>
2024-11-09 09:14:16 +0100

    Carry over all sorts of configs

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: "/felix/",
+    plugins: [],
+    test: {},
+});
