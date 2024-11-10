ab72297854408ce75e30f1b57619337fa6b6e446
Martin Huschenbett <martin.huschenbett@posteo.me>
2024-11-10 09:18:00 +0100

    Import codemirror

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
+import { defaultKeymap } from "@codemirror/commands";
+import { EditorState } from "@codemirror/state";
+import { EditorView, keymap } from "@codemirror/view";
+
 console.log("initializing...");
 
-const editor = document.querySelector<HTMLTextAreaElement>("#editor")!;
-console.assert(editor != null);
-const codeSize = document.querySelector<HTMLSpanElement>("#code-size")!;
-const codeClear = document.querySelector<HTMLButtonElement>("#code-clear")!;
+const ID = {
+    editorContainer: "#editor-container",
+};
 
-function handleEditorInput() {
-    codeSize.textContent = editor.value.length.toString();
-}
-
-function handleCodeClearClick() {
-    editor.value = "";
-}
-
-editor.addEventListener("input", handleEditorInput);
-codeClear.addEventListener("click", handleCodeClearClick);
+const editorContainer = document.querySelector(ID.editorContainer)!;
+console.assert(EditorState !== null, `cannot find ${ID.editorContainer} in DOM.`);
+const startState = EditorState.create({
+    doc: "Hello World",
+    extensions: [keymap.of(defaultKeymap)],
+});
+const _view = new EditorView({
+    state: startState,
+    parent: editorContainer,
+});
