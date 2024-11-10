b60e3cf1498f8589da37a8d1644fef7ce8330fda
Martin Huschenbett <martin.huschenbett@posteo.me>
2024-11-10 10:08:53 +0100

    Template for syntax highlighting

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
-import { defaultKeymap } from "@codemirror/commands";
+import { indentWithTab } from "@codemirror/commands";
+import { indentUnit, LanguageSupport, StreamLanguage } from "@codemirror/language";
 import { EditorState } from "@codemirror/state";
 import { EditorView, keymap } from "@codemirror/view";
+import { minimalSetup } from "codemirror";
+import { pascal as pascalParser } from "./pascal";
 
 console.log("initializing...");
 
 const ID = {
     editorContainer: "#editor-container",
 };
-
 const editorContainer = document.querySelector(ID.editorContainer)!;
 console.assert(EditorState !== null, `cannot find ${ID.editorContainer} in DOM.`);
+
+const pascalLanguage = StreamLanguage.define(pascalParser);
+const pascal = new LanguageSupport(pascalLanguage);
+
 const startState = EditorState.create({
     doc: "Hello World",
-    extensions: [keymap.of(defaultKeymap)],
+    extensions: [minimalSetup, indentUnit.of("    "), keymap.of([indentWithTab]), pascal],
 });
 const _view = new EditorView({
     state: startState,
