0bdc9d1adbc69fd8894df71e7f2d6d47f151dfd1
Martin Huschenbett <martin.huschenbett@posteo.me>
2024-11-10 11:24:22 +0100

    Improve CSS and layout

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,44 @@ import { pascal as pascalParser } from "./pascal";
 
 console.log("initializing...");
 
+const CODE = `while a <> b do  WriteLn('Waiting');
+
+if a > b then WriteLn('Condition met')   {no semicolon allowed before else}
+    else WriteLn('Condition not met');
+
+for i := 1 to 10 do  {no semicolon here as it would detach the next statement}
+    WriteLn('Iteration: ', i);
+
+repeat
+    a := a + 1
+until a = 10;
+
+case i of
+    0 : Write('zero');
+    1 : Write('one');
+    2 : Write('two');
+    3,4,5,6,7,8,9,10: Write('?')
+end;
+
+while a <> b do  WriteLn('Waiting');
+
+if a > b then WriteLn('Condition met')   {no semicolon allowed before else}
+    else WriteLn('Condition not met');
+
+for i := 1 to 10 do  {no semicolon here as it would detach the next statement}
+    WriteLn('Iteration: ', i);
+
+repeat
+    a := a + 1
+until a = 10;
+
+case i of
+    0 : Write('zero');
+    1 : Write('one');
+    2 : Write('two');
+    3,4,5,6,7,8,9,10: Write('?')
+end;`
+
 const ID = {
     editorContainer: "#editor-container",
 };
@@ -17,7 +55,7 @@ const pascalLanguage = StreamLanguage.define(pascalParser);
 const pascal = new LanguageSupport(pascalLanguage);
 
 const startState = EditorState.create({
-    doc: "Hello World",
+    doc: CODE,
     extensions: [minimalSetup, indentUnit.of("    "), keymap.of([indentWithTab]), pascal],
 });
 const _view = new EditorView({
